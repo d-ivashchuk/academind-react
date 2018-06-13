5ac24f33757b7b99311073b2e696f4f50985e722
Dimitri Ivashchuk <dimaiv@me.com>
2018-06-13 12:49:15 +0200

    add styled-components styling

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Button = styled.button`
+padding:5px;
+background-color:#BADA55;
+border-radius:5px;
+color:white;
+font-size:15px;
+cursor:pointer
+:hover{
+  color:#555;
+
+}
+`
+const Cockpit = styled.div`
+background-color:#10ADeD;
+padding:1em;
+
+`
 
 const cockpit = (props)=>{
   return(
-<div>
+<Cockpit>
   <h1>Hello React developers!</h1>
   <p>It's ALIVE!!!</p>
-  <button style={props.style} onClick={props.changed}>
+  <Button style={props.style} onClick={props.changed}>
     Show People
-  </button>
-</div>
+  </Button>
+</Cockpit>
 
   )
 }
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import './Person.css';
+import styled from 'styled-components'
 
+
+const Person = styled.div`
+color:grey;
+width: 60%;
+margin: 16px auto 16px;
+border: 1px solid #eee;
+box-shadow: 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+`
+const Input = styled.input`
+padding:0.3em;
+border-radius:5px;
+color:#ccc;
+font-size:0.7em;
+`
 const person = props => {
-  
+
   return (
-    <div className="Person">
+    <Person>
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} years old
       </p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+      <Input type="text" onChange={props.changed} value={props.name} />
+    </Person>
   );
 };
 
